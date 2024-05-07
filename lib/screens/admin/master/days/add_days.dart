037f288d74dc6b45012ef86202ab9b428a2be027
yashkhokhar28 <yashkhokhar28@gmail.com>
2024-05-07 11:03:20 +0530

    Days Calculation Updated

--- lib/screens/admin/master/days/add_days.dart
+++ lib/screens/admin/master/days/add_days.dart
@@ -1,24 +1,26 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ppmt/components/button.dart';
 import 'package:ppmt/constants/color.dart';
 
 class AddDays extends StatefulWidget {
-  AddDays({Key? key}) : super(key: key);
+  final QueryDocumentSnapshot<Object?>? document;
+
+  const AddDays({Key? key, this.document}) : super(key: key);
 
   @override
   State<AddDays> createState() => _AddDaysState();
 }
 
 class _AddDaysState extends State<AddDays> {
-  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List<Map<String, dynamic>> complexityList = [];
   List<Map<String, dynamic>> levelsList = [];
   List<List<TextEditingController>> controllersList = [];
   List<String> skills = [];
   String? selectedSkill;
+
 
   @override
   void initState() {
@@ -28,8 +30,28 @@ class _AddDaysState extends State<AddDays> {
     fetchSkills();
   }
 
+  void fetchComplexity() async {
+    QuerySnapshot complexitySnapshot =
+        await _firestore.collection('complexity').get();
+
+    setState(() {
+      complexityList = complexitySnapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+    });
+    initializeControllers();
+  }
+
+  void resetControllers() {
+    for (List<TextEditingController> controllers in controllersList) {
+      for (TextEditingController controller in controllers) {
+        controller.text = '0';
+      }
+    }
+  }
+
   void fetchSkills() async {
-    QuerySnapshot skillsSnapshot = await firebaseFirestore
+    QuerySnapshot skillsSnapshot = await _firestore
         .collection('skills')
         .where('isDisabled', isEqualTo: false)
         .get();
@@ -41,21 +63,9 @@ class _AddDaysState extends State<AddDays> {
     setState(() {});
   }
 
-  Future<void> fetchComplexity() async {
-    QuerySnapshot complexitySnapshot =
-        await firebaseFirestore.collection('complexity').get();
 
-    setState(() {
-      complexityList = complexitySnapshot.docs
-          .map((doc) => doc.data() as Map<String, dynamic>)
-          .toList();
-    });
-    initializeControllers();
-  }
-
-  Future<void> fetchLevels() async {
-    QuerySnapshot levelsSnapshot =
-        await firebaseFirestore.collection('levels').get();
+  void fetchLevels() async {
+    QuerySnapshot levelsSnapshot = await _firestore.collection('levels').get();
 
     setState(() {
       levelsList = levelsSnapshot.docs
@@ -75,9 +85,31 @@ class _AddDaysState extends State<AddDays> {
       }
       controllersList.add(controllers);
     }
+
+    if (widget.document != null) {
+      selectedSkill = widget.document!['skillName'] as String?;
+      Map<String, dynamic>? daysData =
+          widget.document!['days'] as Map<String, dynamic>?;
+
+      if (daysData != null) {
+        for (int i = 0; i < levelsList.length; i++) {
+          Map<String, dynamic>? levelData =
+              daysData[levelsList[i]['levelName']] as Map<String, dynamic>?;
+
+          if (levelData != null) {
+            for (int j = 0; j < complexityList.length; j++) {
+              String complexityName =
+                  complexityList[j]['complexityName'] as String;
+              int days = levelData[complexityName] as int? ?? 0;
+              controllersList[i][j].text = days.toString();
+            }
+          }
+        }
+      }
+    }
   }
 
-  void addDaysToDatabase() async {
+  void addOrUpdateDays() async {
     if (selectedSkill == null) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -86,42 +118,35 @@ class _AddDaysState extends State<AddDays> {
       );
       return;
     }
+
+    Map<String, dynamic> daysData = {};
+
     for (int i = 0; i < levelsList.length; i++) {
+      Map<String, dynamic> levelData = {};
+
       for (int j = 0; j < complexityList.length; j++) {
-        String days = controllersList[i][j].text;
-        await firebaseFirestore.collection('days').add({
-          'skill_id': selectedSkill,
-          'level_id': levelsList[i]['levelID'],
-          'complexity_name': complexityList[j]['complexityName'],
-          'days': days,
-        });
+        int days = int.tryParse(controllersList[i][j].text) ?? 0;
+        levelData[complexityList[j]['complexityName'] as String] = days;
       }
+
+      daysData[levelsList[i]['levelName'] as String] = levelData;
     }
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Days added successfully!'),
-      ),
-    );
+    if (widget.document != null) {
+      // Update existing document
+      await _firestore.collection('days').doc(widget.document!.id).update({
+        'skillName': selectedSkill,
+        'days': daysData,
+      });
+    } else {
+      // Add new document
+      await _firestore.collection('days').add({
+        'skillName': selectedSkill,
+        'days': daysData,
+      });
+    }
+
     Navigator.pop(context);
-  }
-
-  void resetControllers() {
-    for (List<TextEditingController> controllers in controllersList) {
-      for (TextEditingController controller in controllers) {
-        controller.text = '0';
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    for (List<TextEditingController> controllers in controllersList) {
-      for (TextEditingController controller in controllers) {
-        controller.dispose();
-      }
-    }
-    super.dispose();
   }
 
   @override
@@ -133,7 +158,7 @@ class _AddDaysState extends State<AddDays> {
         ),
         backgroundColor: kAppBarColor,
         title: Text(
-          "Add Days",
+          widget.document != null ? 'Edit Days' : 'Add Days',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -142,7 +167,9 @@ class _AddDaysState extends State<AddDays> {
         ),
       ),
       body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
               margin: EdgeInsets.all(10),
@@ -169,67 +196,53 @@ class _AddDaysState extends State<AddDays> {
                 ),
               ),
             ),
-            Row(
+            SizedBox(height: 16.0),
+            Table(
+              border: TableBorder.all(),
               children: [
-                Expanded(
-                  child: Container(),
+                TableRow(
+                  children: [
+                    TableCell(
+                      child: Center(child: Text('Levels')),
+                    ),
+                    for (var complexity in complexityList)
+                      TableCell(
+                        child: Center(
+                            child:
+                                Text(complexity['complexityName'] as String)),
+                      ),
+                  ],
                 ),
-                for (var complexity in complexityList)
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(10.0),
-                      child: Text(
-                        complexity['complexityName'] ?? '',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                for (int i = 0; i < levelsList.length; i++)
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(levelsList[i]['levelName'] as String),
                         ),
                       ),
-                    ),
+                      for (int j = 0; j < complexityList.length; j++)
+                        TableCell(
+                          child: TextField(
+                            controller: controllersList[i][j],
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                    ],
                   ),
               ],
             ),
-            for (var level in levelsList)
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(8.0),
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        level['levelName'] ?? '',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  for (int i = 0; i < complexityList.length; i++)
-                    Expanded(
-                      child: Container(
-                        margin: EdgeInsets.all(7.0),
-                        child: SizedBox(
-                          width: 25,
-                          child: TextField(
-                            controller:
-                                controllersList[levelsList.indexOf(level)][i],
-                          ),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: button(
-                textColor: CupertinoColors.white,
-                backgroundColor: CupertinoColors.black,
-                buttonName: "Add Days",
-                onPressed: addDaysToDatabase,
-              ),
-            )
+            SizedBox(height: 16.0),
+            button(
+              backgroundColor: CupertinoColors.black,
+              buttonName: widget.document != null ? 'Update Days' : 'Add Days',
+              textColor: CupertinoColors.white,
+              onPressed: addOrUpdateDays,
+            ),
           ],
         ),
       ),
--- lib/screens/admin/master/days/days_list.dart
+++ lib/screens/admin/master/days/days_list.dart
@@ -1,15 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ppmt/constants/color.dart';
+import 'package:ppmt/screens/admin/master/days/add_days.dart';
 
 class DaysList extends StatefulWidget {
-  const DaysList({super.key});
+  const DaysList({Key? key}) : super(key: key);
 
   @override
   State<DaysList> createState() => _DaysListState();
 }
 
 class _DaysListState extends State<DaysList> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder(
+        stream: _firestore.collection('days').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(color: kAppBarColor),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              var document = snapshot.data!.docs[index];
+              Map<String, dynamic> daysData = document["days"];
+
+              var complexityNames = daysData.entries.first.value.keys.toList();
+
+              List<DataColumn> columns = [
+                DataColumn(label: Text('Levels')),
+                for (var complexityName in complexityNames)
+                  DataColumn(label: Text(complexityName)),
+              ];
+
+              List<DataRow> rows = [];
+              for (var entry in daysData.entries) {
+                List<DataCell> cells = [
+                  DataCell(Text(entry.key)),
+                  for (var subEntry in entry.value.entries)
+                    DataCell(Text('${subEntry.value}')),
+                ];
+                rows.add(DataRow(cells: cells));
+              }
+
+              return ExpansionTile(
+                title: Text(
+                  '${document["skillName"]}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddDays(document: document),
+                          ),
+                        );
+                      },
+                      child: DataTable(
+                        sortAscending: true,
+                        border: TableBorder.all(),
+                        columns: columns,
+                        rows: rows,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
